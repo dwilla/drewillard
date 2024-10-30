323b18e84f48e4904fd207beeb70e34a6cdc728b
Drew Willard <drew@bcalternative.com>
2024-10-30 09:26:33 -0600

    Updates

---
+++ static/js/952.83cf9761.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdrewillard=self.webpackChunkdrewillard||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.83cf9761.chunk.js.map
