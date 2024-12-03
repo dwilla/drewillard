f7ad2e8aadd08970823a3fceea967b81629b52bc
Drews_Dev_Machine <drew@bcalternative.com>
2024-12-03 13:29:41 -0700

    fix deployment errors

--- concert-tickets/src/pages/index.tsx
+++ concert-tickets/src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="text-center space-y-8">
       <h1 className="text-4xl font-bold">Welcome to my website homie!</h1>
-      <p className="text-xl">You can buy tickets here and then I'll appreciate you but only slightly more than I already appreciate you.</p>
+      <p className="text-xl">You can buy tickets here and then I&apos;ll appreciate you but only slightly more than I already appreciate you.</p>
       <div className="space-x-4">
         <Link 
           href="/concerts" 
